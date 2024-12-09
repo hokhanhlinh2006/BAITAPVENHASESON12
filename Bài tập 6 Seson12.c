1e07c8fad9cdde9e796dad71a9b3a828add8259e
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2024-12-10 00:18:46 +0700

    Add files via upload

---
+++ Bài tập 6 Seson12.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool Number(int num) {
+    int sum = 0;
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    if (sum == num) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    int num1, num2;
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &num2);
+
+   
+    if (Number(num1)) {
+        printf("%d la so hoan hao.\n", num1);
+    } else {
+        printf("%d khong phai la so hoan hao.\n", num1);
+    }
+
+    
+    if (Number(num2)) {
+        printf("%d la so hoan hao.\n", num2);
+    } else {
+        printf("%d khong phai la so hoan hao.\n", num2);
+    }
+
+    return 0;
+}
+
